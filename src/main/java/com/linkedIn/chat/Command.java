64776d4375f50757ac57a59638ab897333818608
hania6 <haniayasser@hotmail.com>
2018-02-20 18:37:18 +0200

    added Command Class

--- src/main/java/com/linkedIn/chat/Command.java
+++ src/main/java/com/linkedIn/chat/Command.java
@@ -1,7 +1,14 @@
 package com.linkedIn.chat;
 
+import java.util.HashMap;
+
 public abstract class Command {
 
+	public HashMap hMap;
+
+	public Command(HashMap hMap) {
+		this.hMap=hMap;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
