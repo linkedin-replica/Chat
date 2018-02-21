931342c9d5cda7f3abad254a3060e09e99a9759d
hania6 <haniayasser@hotmail.com>
2018-02-21 19:55:45 +0200

    added encoders and decoders

--- src/main/java/com/linkedIn/chat/models/Message.java
+++ src/main/java/com/linkedIn/chat/models/Message.java
@@ -1,6 +1,18 @@
 package com.linkedIn.chat.models;
 
+import java.io.StringReader;
+import java.util.Collections;
 import java.util.Date;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonReaderFactory;
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
 
 public class Message {
 public String sentFrom;
@@ -17,7 +29,9 @@ public Message(String sentFrom, String sentTo,Date sentDate, boolean read,Date r
 	this.readDate = readDate;
 	this.msg = msg;
 }
-
+public Message () {
+	
+}
 	public String getMsg() {
 	return msg;
 }
@@ -70,5 +84,45 @@ public void setReadDate(Date readDate) {
 		// TODO Auto-generated method stub
 
 	}
+	public static class MessageEncoder implements Encoder.Text<Message> {
+		public void init(EndpointConfig config) {
+		}
+		//@Override
+		public String encode(Message message) throws EncodeException {
+			  return Json.createObjectBuilder()
+                      .add("message", message.getMsg())
+                      .add("sender", message.getSentFrom())
+                      .add("receiver", message.getSentTo())
+                      .add("date", message.getSentDate().toString())
+                      .build().toString();
+		}
+		//@Override
+		public void destroy() {
+		}
+	}
+	public static class MessageDecoder implements Decoder.Text<Message> {
+		private JsonReaderFactory factory = Json
+				.createReaderFactory(Collections.<String, Object> emptyMap());
+		public void init(EndpointConfig config) {
+		}
+		public Message decode(String str) throws DecodeException {
+			Message message = new Message();
+			JsonReader reader = factory.createReader(new StringReader(str));
+			JsonObject json = reader.readObject();
+			message.setSentFrom(json.getString("sender"));
+			message.setSentTo(json.getString("receiver"));
+			message.setMsg(json.getString("message"));
+			message.setSentDate(new Date(json.getString("date")));
+			return message;
+		}
+	
+		public boolean willDecode(String str) {
+			return true;
+		}
+	
+		public void destroy() {
+		}
+	}
+
 
 }
