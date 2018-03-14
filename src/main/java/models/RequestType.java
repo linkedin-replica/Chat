b03907b08018191eb33d8b4ab31915c3d6468d7a
hania6 <haniayasser@hotmail.com>
2018-03-14 21:10:32 +0200

    added skeleton for server

---
+++ src/main/java/models/RequestType.java
@@ -0,0 +1,17 @@
+package models;
+public enum RequestType {
+	USERS("search.user"), 
+	COMPANIES("search.company"), 
+	JOBS("search.job"), 
+	POSTS("search.post");
+	
+	private String commandName;
+	
+	private RequestType(String commandName){
+		this.commandName = commandName;
+	}
+	
+	public String getCommandName(){
+		return commandName;
+	}
+}
